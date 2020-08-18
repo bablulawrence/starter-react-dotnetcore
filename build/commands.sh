e48f59c4228b8abac4b77385768be8044022ae8e
Bablu Lawrence <Bablu.Lawrence@Unilever.com>
2020-08-18 18:26:00 +0530

    Added redis to link template

--- build/commands.sh
+++ build/commands.sh
@@ -15,6 +15,7 @@ dotnet ef migrations remove
 dotnet ef migrations script --project "src\Infrastructure\Infrastructure.csproj" --output "src\Infrastructure\bin\db.sql"
 
 az group deployment create -g starter-app-rg --template-file web-app.json --parameters @web-app-parameters.json 
+az group deployment validate -g starter-app-rg --template-file redis-cache.json --parameters @redis-cache-parameters.json 
 az group deployment validate -g starter-app-rg --template-file key-vault-policy.json --parameters @key-vault-policy-parameters.json 
 az group deployment validate -g starter-app-rg --template-file storage-account.json --parameters @storage-account-parameters.json 
 az group deployment create -g starter-app-rg --template-file link-template.json --parameters @link-template-parameters.json 
