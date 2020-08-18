5d48d86e35fceff3dbbe6866bfd97a297ce5ebc1
Bablu Lawrence <Bablu.Lawrence@Unilever.com>
2020-08-18 18:08:00 +0530

    Included storage account in link template

--- build/commands.sh
+++ build/commands.sh
@@ -16,5 +16,6 @@ dotnet ef migrations script --project "src\Infrastructure\Infrastructure.csproj"
 
 az group deployment create -g starter-app-rg --template-file web-app.json --parameters @web-app-parameters.json 
 az group deployment validate -g starter-app-rg --template-file key-vault-policy.json --parameters @key-vault-policy-parameters.json 
+az group deployment validate -g starter-app-rg --template-file storage-account.json --parameters @storage-account-parameters.json 
 az group deployment create -g starter-app-rg --template-file link-template.json --parameters @link-template-parameters.json 
 az group deployment validate -g starter-app-rg --template-file link-template.json --parameters @link-template-parameters.json 
